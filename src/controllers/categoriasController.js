8038741252ca16557089809d2b2c486ec4f209e3
NachoGalvagno <ignacio.101@hotmail.com>
2021-06-08 22:47:52 -0300

    pequeñas modificaciones

--- src/controllers/categoriasController.js
+++ src/controllers/categoriasController.js
@@ -1,11 +1,15 @@
 const categorias=require('../models/categorias')
+const productos = require('../models/productos')
 
 const controller = {
   categoria: (req, res) => {
     const id = req.params.id
     let categoria = categorias.findByPk(id)
+    let allProducts = productos.findAll()
+    let filterProducts = allProducts.filter()
     res.render('categoria.ejs', { categoria })
   },
+  
 }
   
 module.exports = controller 
