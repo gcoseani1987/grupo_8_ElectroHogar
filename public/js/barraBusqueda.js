4ab28338d591fd8743c157c68e597b6455633150
EmiCucit <emicu144@gmail.com>
2021-08-13 22:26:19 -0300

    Sprint 7 terminado (se agrego la barra de busqueda en el listado)

--- public/js/barraBusqueda.js
+++ public/js/barraBusqueda.js
@@ -1,3 +1,10 @@
 window.onload = function(){
-    
+    let busqueda = document.querySelector('.barra-busqueda');
+    busqueda.addEventListener('focus',()=>{
+        busqueda.style.backgroundColor = 'white';
+    });
+    busqueda.addEventListener('blur',()=>{
+        busqueda.style.backgroundColor = 'rgba(53, 53, 53,0.1)';
+    });
+
 }
