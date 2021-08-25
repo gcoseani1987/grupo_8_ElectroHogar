5f9170a1815f95c9f8a5fddc9c0bfd8bfd622d98
EmiCucit <emicu144@gmail.com>
2021-08-25 17:45:10 -0300

    slider terminado

--- public/js/slider.js
+++ public/js/slider.js
@@ -4,9 +4,40 @@ window.addEventListener('load',()=>{
     const flechaDer = document.querySelector('.arrow-right');
     const marcadores = document.querySelectorAll('.dots')
 
-    console.log(imagenes[0])
+    
+    let i = 0
+    imagenes[i].classList.add('mostrar')
+    marcadores[i].classList.add('active')  
+    
 
     flechaDer.addEventListener('click',()=>{
-         
-    })
+        imagenes[i].classList.remove('mostrar')
+        marcadores[i].classList.remove('active')
+        if(i<(imagenes.length-1)){
+            i = i + 1
+            imagenes[i].classList.add('mostrar')
+            marcadores[i].classList.add('active')
+            
+        }else{
+            i = 0
+            imagenes[i].classList.add('mostrar')
+            marcadores[i].classList.add('active')
+        }    
+    }
+    )
+
+    flechaIzq.addEventListener('click',()=>{
+        imagenes[i].classList.remove('mostrar')
+        marcadores[i].classList.remove('active')
+        if(i != 0){
+            i = i - 1 
+            imagenes[i].classList.add('mostrar')
+            marcadores[i].classList.add('active')
+            
+        }else {
+            i = (imagenes.length-1)
+            imagenes[i].classList.add('mostrar')
+            marcadores[i].classList.add('active')
+        }    
+    })  
 })
