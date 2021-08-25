0da31fb9bf04aaf211ecb1c73f1d0d11939169c7
EmiCucit <emicu144@gmail.com>
2021-08-24 22:25:12 -0300

    Slider a medias

---
+++ public/js/slider.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',()=>{
+    const imagenes = document.querySelectorAll('.imagenes-producto')
+    const flechaIzq = document.querySelector('.arrow-left');
+    const flechaDer = document.querySelector('.arrow-right');
+    const marcadores = document.querySelectorAll('.dots')
+
+    console.log(imagenes[0])
+
+    flechaDer.addEventListener('click',()=>{
+         
+    })
+})
